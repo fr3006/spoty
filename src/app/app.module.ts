49209290b13f6017dc30626d081b3732a1058471
fr3006 <fr3006@gmail.com>
2018-05-06 19:20:17 -0500

    2. tokn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { app_routing } from './components/app.routes';
+
+import { HttpClientModule } from '@angular/common/http';
+//services
+import { SpotifyService } from './services/spotify.service';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
